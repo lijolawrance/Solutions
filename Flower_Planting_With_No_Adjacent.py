71010e9a91c98f1c71e1b3e1dfb111f1f3905a5f
lijolawrance <lijolawrance@gmail.com>
2021-06-17 12:04:36 +0530

    Create Flower_Planting_With_No_Adjacent.py

---
+++ Flower_Planting_With_No_Adjacent.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+
+class Solution:
+    def gardenNoAdj(self, n: int, paths: list[list[int]]) -> list[int]:
+        flowers = [0] * n
+        garden = defaultdict(list)
+        for src, dst in paths:
+            garden[src].append(dst)
+            garden[dst].append(src)
+        for i in range(1, n + 1):
+            colors = {1, 2, 3, 4}
+            for neigh in garden[i]:
+                if flowers[neigh - 1] in colors:
+                    colors.remove(flowers[neigh - 1])
+            flowers[i - 1] = colors.pop()
+        return flowers
