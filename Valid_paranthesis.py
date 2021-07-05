0f75eb759c69624c8dba09e470fd86b1a95fe898
lijolawrance <lijolawrance@gmail.com>
2021-07-05 12:14:55 +0530

    Update Valid_paranthesis.py

--- Valid_paranthesis.py
+++ Valid_paranthesis.py
@@ -1,5 +1,8 @@
+def isValid(s: str) -> bool:
+    while "()" in s or "{}" in s or '[]' in s:
+        s = s.replace("()", "").replace('{}', "").replace('[]', "")
+    return s == ''
+
+
 class Solution:
-    def isValid(self, s: str) -> bool:
-        while "()" in s or "{}" in s or '[]' in s:
-            s = s.replace("()", "").replace('{}', "").replace('[]', "")
-        return s == ''
+    pass
