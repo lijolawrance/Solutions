7fa03c347a48e6fd3b0246493c7a2099b9b46178
lijolawrance <lijolawrance@gmail.com>
2021-07-05 11:56:32 +0530

    code review fixes

--- Letter_Tile_Possibilities.py
+++ Letter_Tile_Possibilities.py
@@ -1,6 +1,9 @@
 import itertools.permutations as permutations
 
 
+def numTilePossibilities(tiles: str) -> int:
+    return len({x for i in range(1, len(tiles) + 1) for x in permutations(tiles, i)})
+
+
 class Solution:
-    def numTilePossibilities(self, tiles: str) -> int:
-        return len({x for i in range(1, len(tiles) + 1) for x in permutations(tiles, i)})
+    pass
--- Long_presses_name.py
+++ Long_presses_name.py
@@ -1,9 +1,12 @@
+def isLongPressedName(name: str, typed: str) -> bool:
+    count = 0
+    for j in enumerate(typed):
+        if count < len(name) and name[count] == typed[j:]:
+            count += 1
+        elif count == 0 or name[count - 1] != typed[j:]:
+            return False
+    return count == len(name)
+
+
 class Solution:
-    def isLongPressedName(self, name: str, typed: str) -> bool:
-        count = 0
-        for j in range(len(typed)):
-            if count < len(name) and name[count] == typed[j]:
-                    count += 1
-            elif count == 0 or name[count-1] != typed[j]:
-                    return False
-        return count == len(name)
+    pass
