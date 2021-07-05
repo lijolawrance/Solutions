ea20926942f31b8ddb484395b757bb70aef2f4e4
lijolawrance <lijolawrance@gmail.com>
2021-07-05 11:23:21 +0530

    Update Best_time_to_buy_sell_stocs_II.py
    code review fixes

--- Best_time_to_buy_sell_stocs_II.py
+++ Best_time_to_buy_sell_stocs_II.py
@@ -1,6 +1,8 @@
-class Solution:
-    def maxProfit(self, prices: list[int],output = 0) -> int:
-        for i in range(1,len(prices)):
-            if(prices[i] > prices[i-1]):
-                output += prices[i] - prices[i-1]
-        return output
+from typing import List
+
+
+def maxProfit(prices: List[int], output=0) -> int:
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i - 1]:
+            output += prices[i] - prices[i - 1]
+    return output
