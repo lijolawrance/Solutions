45134e6d08a1c49d61e2d6b01ce59d44a8f4309d
lijolawrance <lijolawrance@gmail.com>
2021-07-05 11:24:06 +0530

    Update Height_Checker.py
    code review fixes

--- Height_Checker.py
+++ Height_Checker.py
@@ -1,6 +1,9 @@
+from typing import List
+
 import numpy as np
-class Solution:
-    def heightChecker(self, heights: List[int]) -> int:
-        ab = np.array(heights)
-        ab.sort()
-        return sum(heights != ab)
+
+
+def heightChecker(self, heights: List[int]) -> int:
+    ab = np.array(heights)
+    ab.sort()
+    return sum(heights != ab)
