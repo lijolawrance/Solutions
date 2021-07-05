b2c3a6d289530df3d1410641479efe2e97449fff
lijolawrance <lijolawrance@gmail.com>
2021-07-05 12:16:51 +0530

    Update Two_Sum.py

--- Two_Sum.py
+++ Two_Sum.py
@@ -1,6 +1,12 @@
+from typing import List
+
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)):
+        sub= target - nums[i]
+        if sub in nums[i+1:]:
+            return i, nums.index(sub,i+1)
+
+
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for i in range(len(nums)):
-            sub= target - nums[i]
-            if sub in nums[i+1:]:
-                return i, nums.index(sub,i+1)
+    pass
