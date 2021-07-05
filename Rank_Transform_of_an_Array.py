6a596827572d1682894a3e3056f2df7bb871dc43
lijolawrance <lijolawrance@gmail.com>
2021-07-05 11:58:29 +0530

    Update Rank_Transform_of_an_Array.py

--- Rank_Transform_of_an_Array.py
+++ Rank_Transform_of_an_Array.py
@@ -1,4 +1,11 @@
+from numpy import ndarray
 from scipy.stats import rankdata
+from typing import List
+
+
+def arrayRankTransform(arr: List[int]) -> ndarray:
+    return (rankdata(arr, method='dense')).astype(int)
+
+
 class Solution:
-    def arrayRankTransform(self, arr: list[int]) -> list[int]:
-        return (rankdata(arr, method='dense')).astype(int)
+    pass
